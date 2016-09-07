4c6b99bc5c15349c7a39841709c6a2b6a8961ac8
Lichao Wang (Dell laptop) <lichao.wang@uk.imshealth.com>
2016-09-07 18:20:52 +0100

    started re-implementing binaryclassificationevaluatorimspa.py so that
    1. the methods are realised in BinaryClassificationEvaluator_IMSPA;
    2. BinaryClassificationEvaluator_IMSPA is derived from BinaryClassificationEvaluator
    3. The module is separated from tests
    4. other features existing in lichao_test1.py.

--- binaryclassificationevaluatorimspa.py
+++ binaryclassificationevaluatorimspa.py
@@ -1,115 +1,140 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 from abc import abstractmethod, ABCMeta
-
-from pyspark import since
-from pyspark.ml.wrapper import JavaWrapper
-from pyspark.ml.param import Param, Params
-from pyspark.ml.param.shared import HasLabelCol, HasPredictionCol, HasRawPredictionCol
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml.util import keyword_only
 from pyspark.mllib.common import inherit_doc
+from pyspark.ml.param import Param, Params
+from pyspark.ml.param.shared import HasLabelCol, HasRawPredictionCol
+import pyspark.sql.functions as F
+# a temporary solution for SQLContext
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+#
 
-from getPrecisionByRecall_spark import *
+partition_size = 20
 
-__all__ = ['Evaluator', 'BinaryClassificationEvaluator_IMSPA']
+
+def precision_recall_curve(scoreAndLabels, rawPredictionCol, labelCol):
+    # get the tps, fps and thresholds
+    tpsFpsScorethresholds = _binary_clf_curve(scoreAndLabels,
+                                              rawPredictionCol, labelCol)
+    # total tps
+    tpsMax = ((tpsFpsScorethresholds.agg(F.max(F.col("tps"))).collect())[0].asDict())["max(tps)"]
+
+    # calculate precision
+    tpsFpsScorethresholds = tpsFpsScorethresholds \
+        .withColumn("precision", F.col("tps") / (F.col("tps") + F.col("fps")))
+
+    # calculate recall
+    tpsFpsScorethresholds = tpsFpsScorethresholds \
+        .withColumn("recall", F.col("tps") / tpsMax)
+
+    return tpsFpsScorethresholds
+
+
+def _binary_clf_curve(scoreAndLabels, rawPredictionCol, labelCol):
+    # sort the dataframe by pred column in descending order
+    sortedScoresAndLabels = scoreAndLabels.sort(F.desc(rawPredictionCol))
+
+    # creating rank for pred column
+    lookup = (scoreAndLabels.select(rawPredictionCol)
+              .distinct()
+              .sort(F.desc(rawPredictionCol))
+              .rdd
+              .zipWithIndex()
+              .map(lambda x: x[0] + (x[1],))
+              .toDF([rawPredictionCol, "rank"]))
+
+    # join the dataframe with lookup to assign the ranks
+    sortedScoresAndLabels = sortedScoresAndLabels.join(lookup, [rawPredictionCol])
+
+    # sorting in descending order based on the pred column
+    sortedScoresAndLabels = sortedScoresAndLabels.sort(F.desc(rawPredictionCol))
+
+    # adding index to the dataframe
+    sortedScoresAndLabels = sortedScoresAndLabels.rdd.zipWithIndex() \
+        .toDF(['data', 'index']) \
+        .select('data.' + labelCol, 'data.' + rawPredictionCol, 'data.rank', 'index')
+
+    # get existing spark context
+    # sc = SparkContext._active_spark_context
+
+    # get existing HiveContext
+    # sqlContext = HiveContext.getOrCreate(sc)
+
+    # saving the dataframe to temporary table
+    sortedScoresAndLabels.registerTempTable("processeddata")
+
+    # TODO: script to avoid partition by warning, and span data across clusters nodes
+    # creating the cumulative sum for tps
+    # A temporary solution for Spark 1.5.2
+    sqlContext = SQLContext(SparkContext._active_spark_context)
+    sortedScoresAndLabelsCumSum = sqlContext \
+        .sql(
+        "SELECT " + labelCol + ", " + rawPredictionCol + ", rank, index, sum(" + labelCol + ") OVER (ORDER BY index) as tps FROM processeddata ")
+
+    # repartitioning
+    sortedScoresAndLabelsCumSum = sortedScoresAndLabelsCumSum.coalesce(partition_size)
+
+    # cache after partitioning
+    sortedScoresAndLabelsCumSum.cache()
+
+    # droping the duplicates for thresholds
+    sortedScoresAndLabelsCumSumThresholds = sortedScoresAndLabelsCumSum \
+        .dropDuplicates([rawPredictionCol])
+
+    # creating the fps column based on rank and tps column
+    sortedScoresAndLabelsCumSumThresholds = sortedScoresAndLabelsCumSumThresholds \
+        .withColumn("fps", 1 + F.col("rank") - F.col("tps"))
+
+    return sortedScoresAndLabelsCumSumThresholds
+
+
+def nearest_values(df, desired_value):
+    # subtract all the values in the array by desired value & sort by minimum distance on top and take top 2 records.
+    # i.e. get nearest neighbour
+    dfWithDiff = df.withColumn("diff", F.abs(F.col("recall") - desired_value)).sort(F.asc("diff")).take(2)
+    return dfWithDiff
+
+
+def getPrecisionByRecall(scoreAndLabels,
+                         rawPredictionCol,
+                         labelCol,
+                         desired_recall):
+    # get precision, recall, thresholds
+    prcurve = precision_recall_curve(scoreAndLabels, rawPredictionCol, labelCol)
+
+    prcurve_filtered = prcurve.where(F.col('recall') == desired_recall)
+
+    # if the recall value exists then get direct precision corresponding to it
+    if (prcurve_filtered.count() > 0):
+        return ((prcurve_filtered.take(1)[0].asDict())['precision'])
+
+    # if the recall does not exist in the computed values, do nearest neighbour
+    else:
+        prcurve_nearest = nearest_values(prcurve, desired_recall)
+        # prcurve_nearest[0]['index']
+        # prcurve_nearest.cache()
+
+        # indices = prcurve_nearest.select('index').flatMap(lambda x: x).collect()
+
+        diff_value_near1 = (prcurve_nearest[0].asDict())['diff']
+        diff_value_near2 = (prcurve_nearest[1].asDict())['diff']
+
+        precision_near1 = (prcurve_nearest[0].asDict())['precision']
+        precision_near2 = (prcurve_nearest[1].asDict())['precision']
+
+        if (diff_value_near1 > diff_value_near2):
+            return precision_near2
+
+        elif (diff_value_near1 < diff_value_near2):
+            return precision_near1
+
+        elif (diff_value_near1 == diff_value_near2):
+            return (precision_near1 + precision_near2) / 2.0
 
 
 @inherit_doc
-class Evaluator(Params):
-    """
-    Base class for evaluators that compute metrics from predictions.
-
-    .. versionadded:: 1.4.0
-    """
-
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def _evaluate(self, dataset):
-        """
-        Evaluates the output.
-
-        :param dataset: a dataset that contains labels/observations and
-               predictions
-        :return: metric
-        """
-        raise NotImplementedError()
-
-    @since("1.4.0")
-    def evaluate(self, dataset, params=None):
-        """
-        Evaluates the output with optional parameters.
-
-        :param dataset: a dataset that contains labels/observations and
-                        predictions
-        :param params: an optional param map that overrides embedded
-                       params
-        :return: metric
-        """
-        metricDict = (dict((key.name, value) for key, value in params.iteritems()))
-        if params is None:
-            params = dict()
-        if(metricDict['metricName'] == "precisionByRecall"):
-            return getPrecisionByRecall(dataset, metricDict['metricValue'])
-        else:
-            if isinstance(params, dict):
-                if params:
-                    return self.copy(params)._evaluate(dataset)
-                else:
-                    return self._evaluate(dataset)
-            else:
-                raise ValueError("Params must be a param map but got %s." % type(params))
-
-    @since("1.5.0")
-    def isLargerBetter(self):
-        """
-        Indicates whether the metric returned by :py:meth:`evaluate` should be maximized
-        (True, default) or minimized (False).
-        A given evaluator may support multiple metrics which may be maximized or minimized.
-        """
-        return True
-
-
-@inherit_doc
-class JavaEvaluator(Evaluator, JavaWrapper):
-    """
-    Base class for :py:class:`Evaluator`s that wrap Java/Scala
-    implementations.
-    """
-
-    __metaclass__ = ABCMeta
-
-    def _evaluate(self, dataset):
-        """
-        Evaluates the output.
-        :param dataset: a dataset that contains labels/observations and predictions.
-        :return: evaluation metric
-        """
-        self._transfer_params_to_java()
-        return self._java_obj.evaluate(dataset._jdf)
-
-    def isLargerBetter(self):
-        self._transfer_params_to_java()
-        return self._java_obj.isLargerBetter()
-
-
-@inherit_doc
-class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPredictionCol):
+class BinaryClassificationEvaluator_IMSPA(BinaryClassificationEvaluator, HasLabelCol, HasRawPredictionCol):
     """
     Evaluator for binary classification, which expects two input
     columns: rawPrediction and label.
@@ -131,7 +156,8 @@ class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPred
     # a placeholder to make it appear in the generated doc
     metricName = Param(Params._dummy(), "metricName",
                        "metric name in evaluation (areaUnderROC|areaUnderPR)")
-    metricValue = Param(Params._dummy(), "metricValue", "metric recall value in getPrecisionByRecall")
+
+    # metricValue = Param(Params._dummy(), "metricValue", "metric recall value in getPrecisionByRecall")
 
 
     @keyword_only
@@ -142,18 +168,61 @@ class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPred
                  metricName="areaUnderROC")
         """
         super(BinaryClassificationEvaluator_IMSPA, self).__init__()
-        self._java_obj = self._new_java_obj(
-            "org.apache.spark.ml.evaluation.BinaryClassificationEvaluator", self.uid)
-        #: param for metric name in evaluation (areaUnderROC|areaUnderPR)
-        self.metricName = Param(self, "metricName",
-                                "metric name in evaluation (areaUnderROC|areaUnderPR)")
-        self.metricValue = Param(self, "metricValue", "metric recall value in getPrecisionByRecall" )
-        self._setDefault(rawPredictionCol="rawPrediction", labelCol="label",
-                         metricName="areaUnderROC")
-        kwargs = self.__init__._input_kwargs
+        if (metricName == "areaUnderROC") | (metricName == "areaUnderPR"):
+            self._java_obj = self._new_java_obj(
+                "org.apache.spark.ml.evaluation.BinaryClassificationEvaluator", self.uid)
+            #: param for metric name in evaluation (areaUnderROC|areaUnderPR)
+            self.metricName = Param(self, "metricName",
+                                    "metric name in evaluation (areaUnderROC|areaUnderPR)")
+            self._setDefault(rawPredictionCol="rawPrediction", labelCol="label",
+                             metricName="areaUnderROC")
+            kwargs = self.__init__._input_kwargs
+            if "metricValue" in kwargs.keys():
+                kwargs.pop("metricValue")
+
+        elif (metricName == "precisionByRecall"):
+            self.metricValue = Param(self, "metricValue", "metric recall value in getPrecisionByRecall")
+            self.metricName = Param(self, "metricName",
+                                    "metric name in evaluation (areaUnderROC|areaUnderPR)")
+            self._setDefault(rawPredictionCol="rawPrediction", labelCol="label",
+                             metricName="areaUnderROC", metricValue=0.6)
+            kwargs = self.__init__._input_kwargs
+
+        else:
+            raise ValueError("Invalid input metricName: {}".format(self.metricNameValue))
+
         self._set(**kwargs)
 
-    @since("1.4.0")
+        # for the computing precision at given recall in PySpark (in case it's only requested in calling evaluate())
+        self.initMetricValueValue = metricValue
+        self.initMetricNameValue = metricName
+        self.rawPredictionColValue = rawPredictionCol
+        self.labelColValue = labelCol
+
+    def evaluate(self, dataset, params=None):
+        if params is None:
+            if (self.initMetricNameValue == "areaUnderROC") | (self.initMetricNameValue == "areaUnderPR"):
+                return super(BinaryClassificationEvaluator_IMSPA, self).evaluate(dataset)
+            else:
+                return getPrecisionByRecall(dataset,
+                                            self.rawPredictionColValue,
+                                            self.labelColValue,
+                                            self.initMetricValueValue)
+        elif (isinstance(params, dict)):
+            if ("precisionByRecall" in params.keys()):
+                if "metricValue" in params.keys():
+                    return getPrecisionByRecall(dataset,
+                                                self.rawPredictionColValue,
+                                                self.labelColValue,
+                                                params["metricValue"])
+                else:
+                    raise ValueError("When 'precisionByRecall' is specified calling the evaluate() method, " + \
+                                     "'metricValue' must also be specified")
+            else:
+                return super(BinaryClassificationEvaluator_IMSPA, self).evaluate(dataset, params)
+        else:
+            raise ValueError("Params must be a param map but got %s." % type(params))
+
     def setMetricName(self, value):
         """
         Sets the value of :py:attr:`metricName`.
@@ -161,14 +230,12 @@ class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPred
         self._paramMap[self.metricName] = value
         return self
 
-    @since("1.4.0")
     def getMetricName(self):
         """
         Gets the value of metricName or its default value.
         """
         return self.getOrDefault(self.metricName)
 
-    @since("1.4.0")
     def setMetricValue(self, value):
         """
         Sets the value of :py:attr:`metricValue`.
@@ -176,7 +243,6 @@ class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPred
         self._paramMap[self.metricValue] = value
         return self
 
-    @since("1.4.0")
     def getMetricValue(self):
         """
         Gets the value of metricValue or its default value.
@@ -184,9 +250,8 @@ class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPred
         return self.getOrDefault(self.metricValue)
 
     @keyword_only
-    @since("1.4.0")
     def setParams(self, rawPredictionCol="rawPrediction", labelCol="label",
-                  metricName="areaUnderROC", metricValue=0.6):
+                  metricName="areaUnderROC"):
         """
         setParams(self, rawPredictionCol="rawPrediction", labelCol="label", \
                   metricName="areaUnderROC")
@@ -194,29 +259,3 @@ class BinaryClassificationEvaluator_IMSPA(JavaEvaluator, HasLabelCol, HasRawPred
         """
         kwargs = self.setParams._input_kwargs
         return self._set(**kwargs)
-
-
-if __name__ == "__main__":
-    from pyspark.context import SparkContext
-    from pyspark.sql import SQLContext
-    from pyspark import SparkConf, SparkContext
-    from pyspark.sql import SQLContext, HiveContext
-    from pyspark.sql.types import *
-    import decimal
-
-    app_name = "BinaryClassificationEvaluator_IMSPA"
-
-    sc = SparkContext(appName=app_name)
-    sqlContext = HiveContext(sc)
-
-    path = '~/Downloads/Task6__pr_evaluation_metric/toydata/labelPred.csv'
-    scoreAndLabels = sqlContext.read.load(path, format='com.databricks.spark.csv', header='true', inferSchema='true')
-    scoreAndLabels = scoreAndLabels.select(scoreAndLabels.label.cast('double'), scoreAndLabels.pred)
-
-    evaluator = BinaryClassificationEvaluator_IMSPA()
-
-    desiredRecall = decimal.Decimal('0.8')
-    precision = evaluator.evaluate(scoreAndLabels,
-                                   {evaluator.metricName: "precisionByRecall", evaluator.metricValue: desiredRecall})
-
-    print("Precision to given recall is %s" % precision)
